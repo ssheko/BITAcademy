2e779841df1b30a7531da61c39c25dbcd4949fb6
승희 고 <ssheko93@gmail.com>
2019-04-24 09:38:32 +0900

    day15 OODP 수업

---
+++ c++/day15_04.20/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#pragma warning (disable:4996)
+
+using namespace std;	// using 지시문
+using std::cout;		// using 선언
+
+/* 서버, 클라이언트 코드 
+	콜: 클라이언트 -> 서버 호출
+	콜백: 서버 -> 클라이언트 호출
+
+	서버에 클라이언트 정보를 제공하는 방법: 
+		1. 함수 포인터 매개변수를 이용해 콜백 함수의 주소 전달
+		2. 
+*/
+int main()
+{
+	
+}
